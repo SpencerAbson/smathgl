f500cacbc8ee086a74095590dd710e3451e85808
Spencer <spencer.abson@icloud.com>
2022-03-08 00:37:46 +0000

    smgl.h now master headerf

--- include/smathgl.h
+++ include/smathgl.h
@@ -3,16 +3,9 @@
 
 #include <math.h>
 #include "..\source/smgl/vectorf.h"
+#include "..\source/smgl/vectori.h"
 #include "..\source/smgl/matrices.h"
-
-/* general use */
-fvec mult_point_matrix(fvec *in, mat4x4 mat);
-/* Euler rotation  */
-//extern mat4x4 mat4_rotate(const mat4x4 *in, float angle, fvec *unit_vector);
-//extern mat4x4 mat4_rotation(float angle, fvec *unit_vector);
-
-/* lookat and perspective matrices  */
-//extern mat4x4 mat4_perspective(float angle_rad, float aspect_ratio, float near, float far);
-//extern mat4x4 mat4_lookat(fvec *position, fvec *target, fvec *up);
+#include "..\source/smgl/quaternions.h"
+#include "..\source/smgl/matrix_vector_op.h"
 
 #endif // SMATHGL_H_
