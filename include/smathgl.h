98eb887410c521f421b6718447e1248e1cf6d7c8
Spencer <spencer.abson@icloud.com>
2022-03-08 00:31:15 +0000

    moving smgl.c to mv_op.c

--- include/smathgl.h
+++ include/smathgl.h
@@ -8,11 +8,11 @@
 /* general use */
 fvec mult_point_matrix(fvec *in, mat4x4 mat);
 /* Euler rotation  */
-extern mat4x4 mat4_rotate(const mat4x4 *in, float angle, fvec *unit_vector);
-extern mat4x4 mat4_rotation(float angle, fvec *unit_vector);
+//extern mat4x4 mat4_rotate(const mat4x4 *in, float angle, fvec *unit_vector);
+//extern mat4x4 mat4_rotation(float angle, fvec *unit_vector);
 
 /* lookat and perspective matrices  */
-extern mat4x4 mat4_perspective(float angle_rad, float aspect_ratio, float near, float far);
-extern mat4x4 mat4_lookat(fvec *position, fvec *target, fvec *up);
+//extern mat4x4 mat4_perspective(float angle_rad, float aspect_ratio, float near, float far);
+//extern mat4x4 mat4_lookat(fvec *position, fvec *target, fvec *up);
 
 #endif // SMATHGL_H_
