b644ae67507f58586d4a435b5550826dcbd71a52
Spencer <spencer.abson@icloud.com>
2022-03-10 19:37:43 +0000

    cleaning ivec.h

--- source/smgl/vectori.c
+++ source/smgl/vectori.c
@@ -5,46 +5,10 @@
 #include "simd/vectori128.h"
 
 
-void ivec_add(ivec *output, ivec const *input0, ivec const *input1)
-{
-    assert(input0->size == input1->size);
-    output->size = input0->size;
-    output->data.sse_register =  _mm_add_epi32(input0->data.sse_register,
-                                                input1->data.sse_register);
-}
-
-
-void ivec_sub(ivec *output, ivec const *input0, ivec const *input1)
-{
-    assert(input0->size == input1->size);
-    output->size = input0->size;
-    output->data.sse_register =  _mm_sub_epi32(input0->data.sse_register,
-                                                input1->data.sse_register);
-}
-
-
-void ivec_cross(ivec *output, ivec const *input0, ivec const *input1)
-{
-    assert(input0->size == input1->size && input1->size == 3);
-    output->size = input0->size;
-    output->data.sse_register =  vectori128_cross(input0->data.sse_register,
-                                                   input1->data.sse_register);
-}
-
-
 int32_t ivec_dot(ivec const *input0, ivec const *input1)
 {
     assert(input0->size == input1->size);
     return vectori128_dot(input0->data.sse_register, input1->data.sse_register);
-}
-
-
-void ivec_mul(ivec *output, ivec const *input0, ivec const *input1)
-{
-    assert(input0->size == input1->size);
-    output->size = input0->size;
-    output->data.sse_register =  v_mul_i32(input0->data.sse_register,
-                                                    input1->data.sse_register);
 }
 
 
@@ -53,5 +17,4 @@ void ivec_scale(ivec *output, ivec const *input, int32_t scalar)
     __m128i scaling_vec = _mm_set1_epi32(scalar);
     output->size = input->size;
     output->data.sse_register = v_mul_i32(input->data.sse_register, scaling_vec);
-
 }
--- source/smgl/vectori.h
+++ source/smgl/vectori.h
@@ -2,6 +2,7 @@
 #define SMATH_VECTORI_H_
 #include <stdint.h>
 #include "..\..\include/platform.h"
+#include "simd/vectori128.h"
 /* i32 composed vectors, aligned on 16 byte boundary for 128 bit SSE */
 SMGL_ALIGN_16 typedef int32_t ivec4[4];
 
@@ -17,21 +18,19 @@ typedef struct ivec {
 }ivec;
 
 /* i32 composed vector functions  */
-extern void ivec_mul(ivec *out, ivec const *input0, ivec const *input1);
-extern void ivec_cross(ivec *out, ivec const *input0, ivec const *input1); // calc cross product of a vec3
 extern void ivec_scale(ivec *out, ivec const *addr_in, int32_t scalar);
 extern int32_t ivec_dot(ivec const *input0, ivec const *input1); // compute dot product of 2 ivec2/3/4s
 
 /* Vector initers and primitive function macro wrappers */
-#define ivec4_mm_init(vec_out, x, y, z, w)         \
+#define ivec4_mm_init(vec_out, x, y, z, w)      \
     (vec_out)->size = 4;                        \
     (vec_out)->data.sse_register = _mm_set_epi32((w), (z), (y), (x));
 
-#define ivec3_mm_init(vec_out, x, y, z)            \
+#define ivec3_mm_init(vec_out, x, y, z)         \
     (vec_out)->size = 3;                        \
     (vec_out)->data.sse_register = _mm_set_epi32(0, (z), (y), (x));
 
-#define ivec2_mm_init(vec_out, x, y)               \
+#define ivec2_mm_init(vec_out, x, y)            \
     (vec_out)->size = 2;                        \
     (vec_out)->data.sse_register = _mm_set_epi32(0, 0, (y), (x))
 
@@ -44,4 +43,15 @@ extern int32_t ivec_dot(ivec const *input0, ivec const *input1); // compute dot 
     assert((v0).size == (v1).size);             \
     (vec_out).size = (v0).size;                 \
     (vec_out).data.sse_register = _mm_sub_epi((v0).data.sse_register, (v1).data.sse_register)
+
+#define ivec_mm_mul(vec_out, v0, v1)            \
+    assert((v0).size == (v1).size);             \
+    (vec_out).size = (v0).size;                 \
+    (vec_out).data.sse_register = v_mul_i32((v0).data.sse_register, (v1).data.sse_register)
+
+#define ivec_mm_cross(vec_out, v0, v1)          \
+    assert((v0).size == (v1).size && (v0).size == 3);             \
+    (vec_out).size = (v0).size;                 \
+    (vec_out).data.sse_register = vectori128_cross((v0).data.sse_register, (v1).data.sse_register)
+
 #endif // SMATH_SMATH_VECTORI_H_
