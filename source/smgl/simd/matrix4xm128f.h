43eb97aed452518b9bc70d94d4805de828903659
Spencer <spencer.abson@icloud.com>
2022-03-30 23:51:04 +0100

    further rotation optimisation (FMA + 3x3 multiplication)

--- source/smgl/simd/matrix4xm128f.h
+++ source/smgl/simd/matrix4xm128f.h
@@ -134,7 +134,7 @@ static inline void mat4xm128_mul(const __m128 input0[4], const __m128 input1[4],
 }
 
 
-static inline __m128 matrixf128_vec4_mul(__m128 vec, __m128 mat[4])
+static inline __m128 mat4xm128_vec4_mul(__m128 vec, __m128 mat[4])
 {
     __m128 m0 = _mm_mul_ps(vec, mat[0]);
     __m128 m1 = _mm_mul_ps(vec, mat[1]);
@@ -157,9 +157,63 @@ static inline __m128 matrixf128_vec4_mul(__m128 vec, __m128 mat[4])
 }
 
 
+static inline void mat4xm128_rotation_mul(__m128 const input[4], __m128 const rotation[4], __m128 out[4])
+{
+    /* Ignoring the highest component and the last row (treating 4x4 like 3x3)*/
+    {
+        __m128 e0 = _mm_shuffle_ps(rotation[0], rotation[0], _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(rotation[0], rotation[0], _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(rotation[0], rotation[0], _MM_SHUFFLE(2, 2, 2, 2));
+
+        __m128 m0 = _mm_mul_ps(input[0], e0);
+        __m128 m1 = _mm_mul_ps(input[1], e1);
+        __m128 m2 = _mm_mul_ps(input[2], e2);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a2 = _mm_add_ps(a0, m2);
+
+        out[0] = a2;
+    }
+
+    {
+        __m128 e0 = _mm_shuffle_ps(rotation[1], rotation[1], _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(rotation[1], rotation[1], _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(rotation[1], rotation[1], _MM_SHUFFLE(2, 2, 2, 2));
+
+        __m128 m0 = _mm_mul_ps(input[0], e0);
+        __m128 m1 = _mm_mul_ps(input[1], e1);
+        __m128 m2 = _mm_mul_ps(input[2], e2);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a2 = _mm_add_ps(a0, m2);
+
+        out[1] = a2;
+    }
+    {
+        __m128 e0 = _mm_shuffle_ps(rotation[2], rotation[2], _MM_SHUFFLE(0, 0, 0, 0));
+        __m128 e1 = _mm_shuffle_ps(rotation[2], rotation[2], _MM_SHUFFLE(1, 1, 1, 1));
+        __m128 e2 = _mm_shuffle_ps(rotation[2], rotation[2], _MM_SHUFFLE(2, 2, 2, 2));
+
+        __m128 m0 = _mm_mul_ps(input[0], e0);
+        __m128 m1 = _mm_mul_ps(input[1], e1);
+        __m128 m2 = _mm_mul_ps(input[2], e2);
+
+        __m128 a0 = _mm_add_ps(m0, m1);
+        __m128 a2 = _mm_add_ps(a0, m2);
+
+        out[2] = a2;
+    }
+
+    out[3] = input[3];
+}
+
+
+#if SMGL_INSTRSET < 7   // non-fma but assumes SSE 4.1 for now
+
 static inline void mat4xm128_rotate(__m128 const input[4], __m128 const axis, float angle, __m128 out[4])
 {
-    /* SSE 4.2 Matrix rotation from normalized fvec3 and angle (radians) - not pretty */
+    /* SSE 4.1 Matrix rotation from normalized fvec3 and angle (radians) - not pretty
+     * TODO: Optimise like with FMA version */
 
     float const cos_a = cosf(angle);
     float const sin_a = sinf(angle);
@@ -177,7 +231,7 @@ static inline void mat4xm128_rotate(__m128 const input[4], __m128 const axis, fl
         __m128 tmp0 = _mm_shuffle_ps(temp, temp, _MM_SHUFFLE(0, 0, 0, 0));
         __m128 at0  = _mm_mul_ps(tmp0, axis);
         __m128 res0 = _mm_addsub_ps(at0, _mm_shuffle_ps(asic, asic, _MM_SHUFFLE(0, 1, 2, 3)));
-        rotation[0] = _mm_insert_ps(res0, zero, 0x30); // zeroing the garbage values TODO: cleaner approach to this
+        rotation[0] = _mm_insert_ps(res0, zero, 0x30); // zeroing the garbage values TODO: cleaner approach to this -> see FMA
     }
 
     asic = _mm_xor_ps(asic, _mm_set_ps(-0.0f, 0.0f, -0.0f, -0.0f)); // lowest value is negated to reverse previous change
@@ -200,5 +254,46 @@ static inline void mat4xm128_rotate(__m128 const input[4], __m128 const axis, fl
     rotation[3] = _mm_set_ps(1.0f, 0.0f, 0.0f, 0.0f);
     mat4xm128_mul(input, rotation, out);
 }
+
+#else  // FMA
+
+static inline void mat4xm128_rotate(__m128 const input[4], __m128 const axis, float angle, __m128 out[4])
+{
+    /* FMA/SSE Matrix rotation from normalized fvec3 and angle (radians) - less ugly
+     * Identical logic, but with use of fmadd in place of mm_mul and mm_addsub. + consequent change in sign masking */
+
+    float const cos_a = cosf(angle);
+    float const sin_a = sinf(angle);
+
+    __m128 const neg_c = _mm_set1_ps(1.0f - cos_a);
+    __m128 const temp  = _mm_mul_ps(axis, neg_c);
+    __m128 const one   = _mm_set1_ps(1.0f);
+
+    __m128 rotation[4];
+    __m128 asic = _mm_insert_ps(_mm_mul_ps(axis, _mm_set1_ps(sin_a)), _mm_sub_ps(one, neg_c), 0x30); // 1 - (1 - c) = c
+
+    asic = _mm_xor_ps(asic, _mm_set_ps(0.0f, 0.0f, -0.0f, -0.0f));
+    {
+        __m128 tmp0 = _mm_shuffle_ps(temp, temp, _MM_SHUFFLE(0, 0, 0, 0));
+        rotation[0] = _mm_fmadd_ps(tmp0, axis, _mm_shuffle_ps(asic, asic, _MM_SHUFFLE(0, 1, 2, 3)));
+    }
+
+    asic = _mm_xor_ps(asic, _mm_set_ps(0.0f, -0.0f, 0.0f, -0.0f));
+    {
+        __m128 tmp1 = _mm_shuffle_ps(temp, temp, _MM_SHUFFLE(1, 1, 1, 1));
+        rotation[1] = _mm_fmadd_ps(tmp1, axis, _mm_shuffle_ps(asic, asic, _MM_SHUFFLE(0, 0, 3, 2)));
+    }
+
+    asic = _mm_xor_ps(asic, _mm_set_ps(0.0f, 0.0f, -0.0f, -0.0f));
+    {
+        __m128 tmp2 = _mm_shuffle_ps(temp, temp, _MM_SHUFFLE(2, 2, 2, 2));
+        rotation[2]= _mm_fmadd_ps(tmp2, axis, _mm_shuffle_ps(asic, asic, _MM_SHUFFLE(0, 3, 0, 1)));
+    }
+
+    /* set final row and mutliply input by rotation */
+    mat4xm128_rotation_mul(input, rotation, out);
+}
+
+#endif // SMGL_INSTRSET FMA
 
 #endif // MATRIX4XM128F_H_
